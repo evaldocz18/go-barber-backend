3dd8f6e63fc1235a9f0a12c4534c1f1af5208cc9
Evaldo <evaldocz18@gmail.com>
2020-09-07 11:39:06 -0300

    Cria rota de teste

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,4 @@
-import { Router } from 'express';
+import {Router} from 'express';
 import multer from 'multer';
 import multerConfig from './config/multer';
 
@@ -13,28 +13,29 @@ import AvailableController from './app/controllers/AvailableController';
 import authMiddleware from './app/middlewares/auth';
 import ScheduleController from './app/controllers/ScheduleController';
 
-const routes = new Router();
-const upload = multer(multerConfig);
+const routes = new Router ();
+const upload = multer (multerConfig);
 
-routes.post('/users', UserController.store);
-routes.post('/sessions', SessionController.store);
+routes.post ('/users', UserController.store);
+routes.post ('/sessions', SessionController.store);
 
-routes.use(authMiddleware);
+routes.get ('/', (req, res) => res.send ('ok'));
+routes.use (authMiddleware);
 
-routes.put('/users', UserController.update);
+routes.put ('/users', UserController.update);
 
-routes.get('/providers', ProviderController.index);
+routes.get ('/providers', ProviderController.index);
 
-routes.get('/appointments', AppointmentController.index);
-routes.post('/appointments', AppointmentController.store);
-routes.delete('/appointments/:id', AppointmentController.delete);
+routes.get ('/appointments', AppointmentController.index);
+routes.post ('/appointments', AppointmentController.store);
+routes.delete ('/appointments/:id', AppointmentController.delete);
 
-routes.get('/schedule', ScheduleController.index);
-routes.get('/providers/:providerId/available', AvailableController.index);
+routes.get ('/schedule', ScheduleController.index);
+routes.get ('/providers/:providerId/available', AvailableController.index);
 
-routes.get('/notifications', NotificationController.index);
-routes.put('/notifications/:id', NotificationController.update);
+routes.get ('/notifications', NotificationController.index);
+routes.put ('/notifications/:id', NotificationController.update);
 
-routes.post('/files', upload.single('file'), FileController.store);
+routes.post ('/files', upload.single ('file'), FileController.store);
 
 export default routes;
