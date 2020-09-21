e633ed95586f399b456c1b87dc4f9ec078445627
Evaldo <evaldocz18@gmail.com>
2020-09-21 10:01:43 -0300

    adicionado origin para o cors

--- src/app.js
+++ src/app.js
@@ -15,41 +15,45 @@ import sentryConfig from './config/sentry';
 import './database';
 
 class App {
-  constructor() {
-    this.server = express();
-    this.server.use(morgan('dev'));
-    Sentry.init(sentryConfig);
-    this.middlewares();
-    this.routes();
-    this.exceptionHandler();
+  constructor () {
+    this.server = express ();
+    this.server.use (morgan ('dev'));
+    Sentry.init (sentryConfig);
+    this.middlewares ();
+    this.routes ();
+    this.exceptionHandler ();
   }
 
-  middlewares() {
-    this.server.use(Sentry.Handlers.requestHandler());
-    this.server.use(cors()); // this.server.use(cors({ origin: 'https://myapp.com.br' }));
-    this.server.use(express.json());
-    this.server.use(
+  middlewares () {
+    this.server.use (Sentry.Handlers.requestHandler ());
+    this.server.use (
+      cors ({
+        origin: 'http://104.131.75.54:3000',
+      })
+    ); // this.server.use(cors({ origin: 'https://myapp.com.br' }));
+    this.server.use (express.json ());
+    this.server.use (
       '/files',
-      express.static(path.resolve(__dirname, '..', 'tmp', 'uploads'))
+      express.static (path.resolve (__dirname, '..', 'tmp', 'uploads'))
     );
   }
 
-  routes() {
-    this.server.use(routes);
-    this.server.use(Sentry.Handlers.errorHandler());
+  routes () {
+    this.server.use (routes);
+    this.server.use (Sentry.Handlers.errorHandler ());
   }
 
-  exceptionHandler() {
-    this.server.use(async (err, req, res, next) => {
+  exceptionHandler () {
+    this.server.use (async (err, req, res, next) => {
       if (process.env.NODE_ENV === 'development') {
-        const errors = await new Youch(err, req).toJSON();
+        const errors = await new Youch (err, req).toJSON ();
 
-        return res.status(500).json(errors);
+        return res.status (500).json (errors);
       }
 
-      return res.statu(500).json({ error: 'Internal server error' });
+      return res.statu (500).json ({error: 'Internal server error'});
     });
   }
 }
 
-export default new App().server;
+export default new App ().server;
